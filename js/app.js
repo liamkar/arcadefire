fb161e2ce61b53e64c28c0bb14299fb5167eeb45
Ville <villekarjala@gmail.com>
2017-10-08 23:16:18 +0300

    player added to the field

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ let possibleEnemyStartingRows = [40,141,235];
 
 let allEnemies = [];
 
+let player;
+
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
@@ -54,7 +56,54 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    //this.sprite = 'images/char-cat-girl.png';
+    this.sprite = 'images/char-boy.png';
+
+    //ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+
+    //the width of the canvas is 505 pixels, so let's initially pick some from 0 to 505.
+    this.x = x;
+    //as enrmies float on 2nd, 3rd and 4th row of the play area, the y value can be only one of these three:
+    //(expecting that drawing of the enemy image starts from the top left corner of grid position)
+    //101,202,303,
+    this.y = y;
+    //how many pixels in second:
+    //this.speed = speed;
+};
+
+// Update the player's position, required method for game
+Player.prototype.update = function() {
+    /*
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x+(dt*this.speed);
+
+    //no y is needed, as enemies are moving only on x-scale?
+    //this.y = this.y*dt;
+    //but in case enemy overflows the page, let's randomize the y again and set x to starting point.
+    if (this.x > 505) {
+      this.x = -50;
+      this.y = possibleEnemyStartingRows[getRandomIntInclusive(0,2)];
+      this.speed = getRandomIntInclusive(50,500);
+    }
+    */
+
+//ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+//numRows = 6,
+//numCols = 5,
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -75,7 +124,7 @@ for (let enemy=0; enemy<3; enemy++) {
 
   allEnemies.push(new Enemy(startingX, startingY, speed));
 }
-
+player = new Player(2 * 101, 5 * 83);
 
 
 
