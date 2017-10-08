26cfdaf71e3823d44dcbebce06662793f7744a22
Ville <villekarjala@gmail.com>
2017-10-08 22:20:39 +0300

    solving enemy position and speed info

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    //ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+    //as enrmies float on 2nd, 3rd and 4th row of the play area, the y value can be only one of these three:
+    //(expecting that drawing of the enemy image starts from the top left corner of grid position)
+    //101,202,303,
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +22,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x*dt;
+    //this.y = this.y*dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,6 +41,12 @@ Enemy.prototype.render = function() {
 // Place the player object in a variable called player
 
 
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
