384059e59fe4177f05bddbb485cfb783aa0f4f3a
Ville <villekarjala@gmail.com>
2017-10-08 23:24:54 +0300

    prevent player from getting out of area

--- js/app.js
+++ js/app.js
@@ -101,16 +101,16 @@ Player.prototype.update = function() {
 };
 
 Player.prototype.handleInput = function(direction) {
-  if (direction === 'up') {
+  if (direction === 'up' && this.y > 0) {
     this.y = this.y - 83;
   }
-  else if (direction === 'down') {
+  else if (direction === 'down' && this.y < 83*5) {
     this.y = this.y + 83;
   }
-  else if (direction === 'left') {
+  else if (direction === 'left' && this.x > 0) {
     this.x = this.x - 101;
   }
-  else if (direction === 'right') {
+  else if (direction === 'right' && this.x < 101*4) {
     this.x = this.x + 101;
   }
 };
